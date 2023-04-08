4ad6f0015e902946dda01f0331d4f9fa5ac95e06
prakash <prakashsingh01081994@gmail.com>
2023-04-09 02:11:34 +0530

    assignment1 - program1

---
+++ src/Triangle90.java
@@ -0,0 +1,12 @@
+public class Triangle90 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            int j = 0;
+            while (j <= i) {
+                System.out.print(j);
+                j++;
+            }
+            System.out.println();
+        }
+    }
+}
